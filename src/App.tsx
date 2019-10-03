3dce3c04c5e02ddd864aeb249af1d565bb60a167
Aaron <AaronBatilo@gmail.com>
2019-10-03 16:38:22 -0600

    Less code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,21 @@ import winkTokenizer from 'wink-tokenizer';
 import tokenizer from 'sbd';
 import syllables from 'syllable';
 import spache from 'spache';
-import { asyncComponent } from 'react-async-component';
 import daleChall from 'dale-chall';
+
+import GunningFog from './GunningFog';
+import AutomatedReadability from './AutomatedReadability';
+import ColemanLiau from './ColemanLiau';
+import DaleChall from './DaleChall';
+import Flesch from './Flesch';
+import FleschKincaid from './FleschKincaid';
+import SMOG from './SMOG';
+import Spache from './Spache';
 
 const defaultText = `In computer science, radix sort is a non-comparative sorting algorithm. It avoids comparison by creating and distributing elements into buckets according to their radix. For elements with more than one significant digit, this bucketing process is repeated for each digit, while preserving the ordering of the prior step, until all digits have been considered. For this reason, radix sort has also been called bucket sort and digital sort.
 
 Radix sort can be applied to data that can be sorted lexicographically, be they
 integers, words, punch cards, playing cards, or the mail.`;
-
-const AsyncGunningFog = asyncComponent({
-                               resolve: () => import('./GunningFog')});
-const AsyncAutomatedReadability = asyncComponent({
-                               resolve: () => import('./AutomatedReadability')});
-const AsyncColemanLiau = asyncComponent({
-                               resolve: () => import('./ColemanLiau')});
-const AsyncDaleChall = asyncComponent({
-                               resolve: () => import('./DaleChall')});
-const AsyncFlesch = asyncComponent({
-                               resolve: () => import('./Flesch')});
-const AsyncFleschKincaid = asyncComponent({
-                               resolve: () => import('./FleschKincaid')});
-const AsyncSMOG = asyncComponent({
-                               resolve: () => import('./SMOG')});
-const AsyncSpache = asyncComponent({
-                               resolve: () => import('./Spache')});
 
 const App: React.FC = () => {
   const [words, setWords] = useState(defaultText);
@@ -62,14 +53,14 @@ const App: React.FC = () => {
   return (
     <div>
       <textarea value={words} onChange={(event) => setWords(event.target.value)} />
-      <AsyncGunningFog {...params} />
-      <AsyncAutomatedReadability {...params} />
-      <AsyncColemanLiau {...params} />
-      <AsyncDaleChall {...params} />
-      <AsyncFlesch {...params} />
-      <AsyncFleschKincaid {...params} />
-      <AsyncSMOG {...params} />
-      <AsyncSpache {...params} />
+      <GunningFog {...params} />
+      <AutomatedReadability {...params} />
+      <ColemanLiau {...params} />
+      <DaleChall {...params} />
+      <Flesch {...params} />
+      <FleschKincaid {...params} />
+      <SMOG {...params} />
+      <Spache {...params} />
     </div>
   );
 }
