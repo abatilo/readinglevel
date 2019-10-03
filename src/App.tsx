ab18ede738d0d1c43dfe6e90a90863ec68e32f8f
Aaron <AaronBatilo@gmail.com>
2019-10-02 21:53:12 -0600

    Add automated readability, coleman liau, and dale chall

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,45 @@ import winkTokenizer from 'wink-tokenizer';
 import tokenizer from 'sbd';
 import syllables from 'syllable';
 import gunningFog from 'gunning-fog';
+import automatedReadability from 'automated-readability';
+import colemanLiau from 'coleman-liau';
+import daleChall from 'dale-chall';
+import daleChallFormula from 'dale-chall-formula';
 
 const App: React.FC = () => {
   const [words, setWords] = useState("");
 
-  const wordCount: number = new winkTokenizer().tokenize(words).length;
+  const characterCount = words.replace(/\W/g, "").length;
+  const tokens = new winkTokenizer().tokenize(words).filter(t => t.tag === "word").map(t => t.value.toLowerCase());
+  const wordCount: number = tokens.length;
+
+  const hardWords = tokens.filter(t => !daleChall.includes(t));
+  const hardWordCount: number = hardWords.length;
+
   const sentenceCount: number = tokenizer.sentences(words).length;
   const syllableCount: number = syllables(words);
-  const gunningFogScore: number = gunningFog({sentence:sentenceCount, word: wordCount});
+
+  const gunningFogScore: number = Math.ceil(gunningFog({sentence:sentenceCount, word: wordCount}));
+  const automatedReadabilityScore: number = Math.ceil(automatedReadability({sentence:sentenceCount, word: wordCount, character: characterCount}));
+  const colemanLiauScore: number = Math.ceil(colemanLiau({sentence:sentenceCount, word: wordCount, letter: characterCount}));
+
+  const daleChallFormulaScore: number[] = daleChallFormula({word: wordCount, sentence: sentenceCount, difficultWord: hardWordCount});
+  const daleChallFormulaGrade: number = daleChallFormula.gradeLevel(daleChallFormulaScore);
 
   return (
     <div>
       <textarea onChange={(event) => setWords(event.target.value)} />
       <div>
-        Word count: {wordCount}
-      </div>
-      <div>
-        Sentence count: {sentenceCount}
-      </div>
-      <div>
-        Syllables: {syllableCount}
-      </div>
-      <div>
         Gunning Fog: {gunningFogScore}
+      </div>
+      <div>
+        Automated readability: {automatedReadabilityScore}
+      </div>
+      <div>
+        Coleman Liau: {colemanLiauScore}
+      </div>
+      <div>
+        Dale Chall: {daleChallFormulaScore}
       </div>
     </div>
   );
--- src/types/module.d.ts
+++ src/types/module.d.ts
@@ -14,3 +14,38 @@ declare module 'gunning-fog' {
   }
   export default function gunningFog(value: GunningFogCounts): number;
 }
+
+declare module 'automated-readability' {
+  type AutomatedReadabilityCounts = {
+    sentence: number;
+    word: number;
+    character: number;
+  }
+  export default function automatedReadability(value: AutomatedReadabilityCounts): number;
+}
+
+declare module 'coleman-liau' {
+  type ColemanLiauCounts = {
+    sentence: number;
+    word: number;
+    letter: number;
+  }
+  export default function colemanLiau(value: ColemanLiauCounts): number;
+}
+
+declare module 'dale-chall-formula' {
+  type DaleChallFormulaCounts = {
+    sentence: number;
+    word: number;
+    difficultWord?: number;
+  }
+
+  interface GradeLevelFunction {
+    (value: DaleChallFormulaCounts): number[];
+    gradeLevel(score: number[]): number;
+  }
+
+  declare const daleChallFormula: GradeLevelFunction;
+
+  export default daleChallFormula;
+}
