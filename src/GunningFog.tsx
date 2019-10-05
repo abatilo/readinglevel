097b09e92d9cd77def5eff01b349274f722a289c
Aaron <AaronBatilo@gmail.com>
2019-10-05 10:39:00 -0600

    Remove colon in GunningFog

--- src/GunningFog.tsx
+++ src/GunningFog.tsx
@@ -11,7 +11,7 @@ const GunningFog = (props: GunningFogProps) => {
   const gunningFogScore: number = Math.floor(gunningFog({...props}));
   return (
     <tr>
-      <td>Gunning Fog:</td>
+      <td>Gunning Fog</td>
       <td>{gunningFogScore}</td>
     </tr>
   );
