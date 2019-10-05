8d0c6b2e8c94bd77b95f6cc59506eeabfe69cad2
Aaron <AaronBatilo@gmail.com>
2019-10-05 10:00:05 -0600

    Add Google Analytics

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
+import ReactGA from 'react-ga';
 
+ReactGA.initialize('UA-149336980-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
