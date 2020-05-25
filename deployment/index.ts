fa3c34e244533514530c025254964928faee04c0
Aaron <AaronBatilo@gmail.com>
2020-05-25 12:49:49 -0600

    Try passing provider

--- deployment/index.ts
+++ deployment/index.ts
@@ -13,8 +13,6 @@ const appName = 'readinglevel';
 const repository = new awsx.ecr.Repository(appName);
 const image = repository.buildAndPushImage('../');
 
-const appLabels = { app: appName };
-
 const pod = new kx.PodBuilder({
   containers: [
     {
@@ -23,58 +21,70 @@ const pod = new kx.PodBuilder({
     },
   ],
 });
-const deployment = new kx.Deployment(appName, {
-  spec: pod.asDeploymentSpec({
-    strategy: { rollingUpdate: { maxUnavailable: 0 } },
-  }),
-});
+const deployment = new kx.Deployment(
+  appName,
+  {
+    spec: pod.asDeploymentSpec({
+      strategy: { rollingUpdate: { maxUnavailable: 0 } },
+    }),
+  },
+  { provider: k8sProvider }
+);
 const service = deployment.createService();
 
-const ingressMiddleware = new k8s.apiextensions.CustomResource(appName, {
-  apiVersion: 'traefik.containo.us/v1alpha1',
-  kind: 'Middleware',
-  metadata: {},
-  spec: {
-    headers: {
-      forceSTSHeader: true,
-      stsSeconds: 31536000,
-      stsIncludeSubdomains: true,
-      stsPreload: true,
-      referrerPolicy: 'no-referrer-when-downgrade',
-      contentTypeNosniff: true,
-      contentSecurityPolicy: 'upgrade-insecure-requests',
-      browserXssFilter: true,
-      customFrameOptionsValue: 'SAMEORIGIN',
-      customResponseHeaders: {
-        'Feature-Policy':
-          'geolocation none; midi none; notifications none; push none; sync-xhr none; microphone none; camera none; magnetometer none; gyroscope none; speaker self; vibrate none; fullscreen self; payment none;',
+const ingressMiddleware = new k8s.apiextensions.CustomResource(
+  appName,
+  {
+    apiVersion: 'traefik.containo.us/v1alpha1',
+    kind: 'Middleware',
+    metadata: {},
+    spec: {
+      headers: {
+        forceSTSHeader: true,
+        stsSeconds: 31536000,
+        stsIncludeSubdomains: true,
+        stsPreload: true,
+        referrerPolicy: 'no-referrer-when-downgrade',
+        contentTypeNosniff: true,
+        contentSecurityPolicy: 'upgrade-insecure-requests',
+        browserXssFilter: true,
+        customFrameOptionsValue: 'SAMEORIGIN',
+        customResponseHeaders: {
+          'Feature-Policy':
+            'geolocation none; midi none; notifications none; push none; sync-xhr none; microphone none; camera none; magnetometer none; gyroscope none; speaker self; vibrate none; fullscreen self; payment none;',
+        },
       },
     },
   },
-});
+  { provider: k8sProvider }
+);
 
-const ingressRoute = new k8s.apiextensions.CustomResource(appName, {
-  apiVersion: 'traefik.containo.us/v1alpha1',
-  kind: 'IngressRoute',
-  metadata: {},
-  spec: {
-    entryPoints: ['websecure'],
-    routes: [
-      {
-        match: 'Host(`www.readinglevel.app`)',
-        kind: 'Rule',
-        middlewares: [
-          {
-            name: ingressMiddleware.metadata.name,
-          },
-        ],
-        services: [
-          {
-            name: service.metadata.name,
-            port: service.spec.ports[0].port,
-          },
-        ],
-      },
-    ],
+const ingressRoute = new k8s.apiextensions.CustomResource(
+  appName,
+  {
+    apiVersion: 'traefik.containo.us/v1alpha1',
+    kind: 'IngressRoute',
+    metadata: {},
+    spec: {
+      entryPoints: ['websecure'],
+      routes: [
+        {
+          match: 'Host(`www.readinglevel.app`)',
+          kind: 'Rule',
+          middlewares: [
+            {
+              name: ingressMiddleware.metadata.name,
+            },
+          ],
+          services: [
+            {
+              name: service.metadata.name,
+              port: service.spec.ports[0].port,
+            },
+          ],
+        },
+      ],
+    },
   },
-});
+  { provider: k8sProvider }
+);
