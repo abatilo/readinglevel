58b6bca2a7c12ea3333b22168edc7d5b88b94306
Aaron <AaronBatilo@gmail.com>
2020-05-25 12:15:15 -0600

    Switch to kx

--- deployment/index.ts
+++ deployment/index.ts
@@ -1,5 +1,6 @@
 import * as awsx from '@pulumi/awsx';
 import * as k8s from '@pulumi/kubernetes';
+import * as kx from '@pulumi/kubernetesx';
 import * as pulumi from '@pulumi/pulumi';
 
 const clusterStackRef = new pulumi.StackReference('prod');
@@ -14,60 +15,25 @@ const image = repository.buildAndPushImage('../');
 
 const appLabels = { app: appName };
 
-const deployment = new k8s.apps.v1.Deployment(
-  appName,
-  {
-    metadata: { labels: appLabels },
-    spec: {
-      strategy: {
-        rollingUpdate: {
-          maxUnavailable: 0,
-        },
-      },
-      replicas: 2,
-      selector: { matchLabels: appLabels },
-      template: {
-        metadata: { labels: appLabels },
-        spec: {
-          containers: [
-            {
-              name: appName,
-              image,
-              ports: [{ name: 'http', containerPort: 80 }],
-            },
-          ],
-        },
-      },
+const pod = new kx.PodBuilder({
+  containers: [
+    {
+      image,
+      ports: { http: 80 },
     },
-  },
-  { provider: k8sProvider }
-);
-
-const service = new k8s.core.v1.Service(
-  appName,
-  {
-    metadata: { labels: appLabels },
-    spec: {
-      selector: appLabels,
-      type: 'ClusterIP',
-      ports: [
-        {
-          name: 'http',
-          port: 80,
-          targetPort: 80,
-        },
-      ],
-    },
-  },
-  { provider: k8sProvider }
-);
+  ],
+});
+const deployment = new kx.Deployment(appName, {
+  spec: pod.asDeploymentSpec({
+    strategy: { rollingUpdate: { maxUnavailable: 0 } },
+  }),
+});
+const service = deployment.createService();
 
 const ingressMiddleware = new k8s.apiextensions.CustomResource(appName, {
   apiVersion: 'traefik.containo.us/v1alpha1',
   kind: 'Middleware',
-  metadata: {
-    labels: appLabels,
-  },
+  metadata: {},
   spec: {
     headers: {
       forceSTSHeader: true,
@@ -90,9 +56,7 @@ const ingressMiddleware = new k8s.apiextensions.CustomResource(appName, {
 const ingressRoute = new k8s.apiextensions.CustomResource(appName, {
   apiVersion: 'traefik.containo.us/v1alpha1',
   kind: 'IngressRoute',
-  metadata: {
-    labels: appLabels,
-  },
+  metadata: {},
   spec: {
     entryPoints: ['websecure'],
     routes: [
