1cfbaa9689675883773138187ff87073ee1d7874
Aaron <AaronBatilo@gmail.com>
2021-04-09 21:12:40 -0600

    Reduce default text

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,11 +8,7 @@ const tok = new Tokenizer("readinglevel");
 
 const defaultText = `ReadingLevel.app will give you a rough estimate at the required level of education necessary for someone to read and comprehend your body of text.
 
-This can be used to ensure that your speech is easy to understand for your audience, or to make sure that some passage is appropriate to read to your class. There are many reasons that you might want to know what grade level you're communicating at.
-
-To use this web app, just replace this text with whatever you want to analyze. The grade level estimates will update in real time.
-
-ReadingLevel.app implements all of the most commonly used algorithms. By hovering over each algorithm name, you can read a brief summary about what each algorithm is measuring.`;
+To use this web app, just replace this text with whatever you want to analyze.`;
 
 const App: React.FC = () => {
   const [content, setContent] = useState(defaultText);
